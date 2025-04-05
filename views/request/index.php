5d1574184c8f1bf2c88410f3b22e3b15340fe141
John Doe <username@mail.com>
2025-04-04 19:59:31 -0700

    all done

--- views/request/index.php
+++ views/request/index.php
@@ -78,33 +78,66 @@ $this->params['breadcrumbs'][] = $this->title;
             }
         ]);
     } else if (!Yii::$app->user->isGuest && Yii::$app->user->identity->isAdmin()) {
-        echo GridView::widget([
+        echo ListView::widget([
             'dataProvider' => $dataProvider,
-            'columns' => [
-                ['class' => 'yii\grid\SerialColumn'],
-                'id',
-                'user_id',
-                'service_id',
-                'address',
-                'contact_phone',
-                //'desired_datetime',
-                //'custom_service_description',
-                //'payment_method',
-                'status',
-                //'cancellation_reason',
-                [
-                    'class' => ActionColumn::className(),
-                    'template' => '{update}',
-                    'urlCreator' => function ($action, Request $model, $key, $index, $column) {
-                        return Url::toRoute([$action, 'id' => $model->id]);
-                    },
-                    'buttons' => [
-                        'update' => function ($url) {
-                            return Html::a('<span class="btn btn-primary" type="button">Ответить на заявку</span>', $url);
-                        },
-                    ],
-                ],
-            ],
+            'options' => ['class' => 'row g-4'],
+            'itemOptions' => ['class' => 'col-md-4'],
+            'itemView' => function ($model, $key) {
+                $service_name = Service::findOne($model->service_id)->name
+                    ?? $model->custom_service_description." (Своя услуга)";
+                $payment = ['cash' => 'Наличные', 'card' => 'Банковская карта'];
+                $statuses = ["new" => ["name" => "Новая", "class" => "bg-secondary bg-gradient"],
+                    "in_progress" => ["name" => "В работе", "class" => "bg-primary bg-gradient"],
+                    "cancelled" => ["name" => "Отменено", "class" => "bg-danger bg-gradient"],
+                    "completed" => ["name" => "Выполнено", "class" => "bg-success bg-gradient"]
+                ];
+                $current_status = $model->status == "cancelled"
+                    ? $statuses[$model->status]["name"]. ' (' . $model->cancellation_reason . ')'
+                    : $statuses[$model->status]["name"];
+                $answer = ($model->status === "new" || $model->status === "in_progress") ?
+                    Html::a('&#128584;', ['update', 'id' => $model->id], [
+                    'class' => 'btn btn-dark p-1 text-white']) : "";
+                return '
+                <div class="card shadow-sm">
+                    <div class="card-header '
+                    . $statuses[$model->status]["class"] .
+                    ' text-white d-flex justify-content-between">
+                        <h5 class="card-title">Заявка № ' . Html::encode($key) . '</h5>
+                        '. $answer .'
+                    </div>
+                    <div class="card-body">
+                        <p class="d-flex justify-content-between">
+                            <b>ФИО: </b>'
+                    . app\models\User::findOne($model->user_id)->full_name .
+                    '</p>
+                        <p class="d-flex justify-content-between">
+                            <b>Услуга: </b>'
+                    . $service_name .
+                    '</p>
+                        <p class="d-flex justify-content-between">
+                            <b>Адрес: </b>'
+                    . $model->address .
+                    '</p>
+                        <p class="d-flex justify-content-between">
+                            <b>Телефон: </b>'
+                    . $model->contact_phone .
+                    '</p>
+                        <p class="d-flex justify-content-between">
+                            <b>Желательное время: </b>'
+                    . date('d-m-Y H:i', strtotime($model->desired_datetime)) .
+                    '</p>
+                        <p class="d-flex justify-content-between">
+                            <b>Способ оплаты: </b>'
+                    . $payment[$model->payment_method] .
+                    '</p>
+                    </div>
+                    <div class="card-footer d-flex justify-content-between">
+                        <b>Статус: </b>'
+                    . $current_status .
+                    '</div>
+                </div>
+            ';
+            }
         ]);
     } ?>
 </div>
